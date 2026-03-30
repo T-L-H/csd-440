a132daecf7dd625a1d0ae276f49efbdffdb668ad
T-L-H <tlhanley0803@gmail.com>
2026-03-29 23:57:15 -0400

    Week 1

---
+++ module-1/HanleyFirstProgram.php
@@ -0,0 +1,40 @@
+<?php
+// Name: Tevyah Hanley
+// Date: March 29, 2026
+// Assignment: Module 1.3
+// Purpose: Demonstrate your basic PHP site.
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Program</title>
+</head>
+<body>
+
+<h1>Welcome to My PHP Page</h1>
+
+<?php
+// ----------------------------------------
+// PHP Code Snippet #1
+// Displays a welcome message to the user
+// ----------------------------------------
+
+echo "<p>Hello, this is my first PHP program!</p>";
+?>
+
+<?php
+// ----------------------------------------
+// PHP Code Snippet #2
+// Performs a simple addition calculation
+// ----------------------------------------
+
+$firstNumber = 10;
+$secondNumber = 5;
+$sum = $firstNumber + $secondNumber;
+
+echo "<p>The sum of $firstNumber and $secondNumber is: $sum</p>";
+?>
+
+</body>
+</html>
